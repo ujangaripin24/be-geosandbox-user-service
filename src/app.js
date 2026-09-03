95b44945ee056c5dc821ea290c5f2ae695543ded
ujangaripin24 <ujangarifin06@gmail.com>
2026-09-03 21:11:30 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const cors = require("cors");
+const express = require("express")
+const logger = require("morgan")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const createError = require("http-errors")
+const fs = require("fs")
+const path = require("path")
+
+dotenv.config();
+
+let app = express();
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'))
+let dateNow = new Date().toISOString().replace('T', ' ').substring(0, 19);
+
+app.use(helmet());
+app.use(cors());
+app.use(logger('dev'));
+app.use(logger('combined', { stream: accessLogStream }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        message: "[SERVICE-USER] Server Berhasil Berjalan",
+        date: dateNow
+    });
+});
+
+app.listen(process.env.APP_PORT, async () => {
+    console.log(`[SERVICE-USER] Server berjalan di port ${process.env.APP_PORT}`);
+    try {
+        console.log("Connected!")
+    } catch (error) {
+        console.error("❌ Unable to start server:");
+        console.error(error.message);
+        process.exit(1);
+    }
+});
+
+module.exports = app
